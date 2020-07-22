efce696d9d8b22ec8bab4af47d094bae16c4d23e
Kiril <kiril.bogdan.97@gmail.com>
2020-07-23 02:14:33 +0300

    Initial Commit

--- app/js/app.js
+++ app/js/app.js
@@ -4,25 +4,13 @@ $(document).ready(function () {
     $('body').toggleClass('lock');
   });
 
-  $('.header__slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: '<a class="header__slider-prev" href="#">prev</a>',
-    nextArrow: '<a class="header__slider-next" href="#">next</a>',
-    appendArrows: '.header__middle',
-    responsive: [
-      {
-        breakpoint: 580,
-        settings: {
-          appendArrows: '.arrow-wrapp',
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          // slidesToShow: 1
-        }
-      }
-    ]
-  })
+
+
+
+
+
+
+
+
+
 });
